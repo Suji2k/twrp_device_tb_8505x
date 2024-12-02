7e40e72ea5ecb0f5c5a3c3ffb93dad778413321c
Suji2k <82132754+Suji2k@users.noreply.github.com>
2024-12-02 23:55:59 +0530

    Update Android.mk

--- recovery/root/Android.mk
+++ recovery/root/Android.mk
@@ -1,1 +1,10 @@
+# Replace $$DEVICE$$ with your Device Name's Value. Mine is Primo_RX5.
+# Replace $$BRAND$$ with your Brand's / Manufacturer's Value, Mine is WALTON 
 
+ifneq ($(filter tb-8505x,$(TARGET_DEVICE)),)
+
+LOCAL_PATH := device/Lenovo/tb-8505x
+
+include $(call all-makefiles-under,$(LOCAL_PATH))
+
+endif
